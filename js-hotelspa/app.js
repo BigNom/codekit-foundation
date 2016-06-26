4555404c571b00f1107962cfb7e9cd394b74f4a9
[BigNom] <[simon.biddle@live.com]>
2016-06-26 22:29:24 +0800

    Sunday commit

---
+++ js-hotelspa/app.js
@@ -0,0 +1,21 @@
+function getHeight() {
+  var height = $('section.promo').height();
+$('#overlay').css({'height':height+'px'})
+}
+
+$(function() {
+  getHeight();
+
+  $('#gallery a').on('click', function() {
+    var image = $(this).find('img').attr('src');
+    var imageSelected = image.split('thumbnail');
+    var fullPath = imageSelected[0] + 'fullsize' + imageSelected[1];
+    $('#galleryModal #imageContainer').attr('src', fullPath);
+  });
+});
+
+$(window).resize(function() {
+  getHeight();
+});
+
+$(document).foundation();
